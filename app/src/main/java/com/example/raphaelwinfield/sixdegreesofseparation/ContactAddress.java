6439507c66ed273f8c79180214aed9e5ea75e51a
Raphael Winfield <RaphaelWinfield@outlook.com>
2018-05-18 23:05:59 +0800

    5.14

---
+++ app/src/main/java/com/example/raphaelwinfield/sixdegreesofseparation/ContactAddress.java
@@ -0,0 +1,13 @@
+package com.example.raphaelwinfield.sixdegreesofseparation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_address);
+    }
+}
